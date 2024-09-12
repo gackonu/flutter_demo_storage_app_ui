6a9a246bb8e9a4b9b22f9b04640190de99c37ee6
gackonu <gackonu111@gmail.com>
2024-09-12 21:52:44 +0000

    Homepage buttom nav bar added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:storage_app/pages/home.dart';
 import 'package:storage_app/pages/home_page.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: Home(),
     );
   }
 }
---
+++ lib/pages/home.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:storage_app/pages/chat_page.dart';
+import 'package:storage_app/pages/home_page.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int currentpage = 0;
+  List<Widget> pages = const [HomePage(), ChatPage()];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: currentpage,
+        children: pages,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.blueAccent,
+        shape: const CircleBorder(),
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        onTap: (value) {
+          setState(() {
+            currentpage = value;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.timer_outlined,
+              ),
+              // activeIcon: Icon(Icons.timer_outlined, color: Colors.blueAccent,),
+              label: ''),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.pie_chart_outline_outlined,
+              ),
+              // activeIcon: Icon(Icons.timer_outlined, color: Colors.blueAccent,),
+              label: '')
+        ],
+      ),
+    );
+  }
+}
